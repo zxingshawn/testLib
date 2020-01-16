979c212b102b29d2723baa7051f3a0336e6516ac
Shang Zhenxing <zxingshawn@outlook.com>
2020-01-16 15:29:53 +0800

    1

---
+++ Classes/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  Interview02-GCD定时器
+//
+//  Created by Simon on 2020/1/16.
+//  Copyright © 2020 MJ Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+-(void)test;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/Person.m
@@ -0,0 +1,16 @@
+//
+//  Person.m
+//  Interview02-GCD定时器
+//
+//  Created by Simon on 2020/1/16.
+//  Copyright © 2020 MJ Lee. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+-(void)test{
+    
+    NSLog(@"我的库");
+}
+@end
